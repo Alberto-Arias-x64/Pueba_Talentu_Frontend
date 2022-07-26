1d560cb566cad90cbb5c577d12344316a0125f85
Alberto_x64 <alberto.arias.x64@gmail.com>
2022-07-26 01:11:59 -0500

    Alpha ready

--- main.js
+++ main.js
@@ -1,11 +1,17 @@
 const form = document.querySelectorAll('form')[0]
+const date_now = document.getElementsByName('date_of_birthday')[0]
+
+date_now.max = new Date(new Date().getTime() - new Date().getTimezoneOffset() * 60000).toISOString().split("T")[0];
 
 window.fetch('https://reqres.in/api/users?page=1').then(response => { //brings data from the public API
     response.json().then(({ data }) => {
-        Print_Table(data)
         if(window.localStorage.key('data_users')){
             const local_data = JSON.parse(window.localStorage.getItem('data_users'))
             Print_Table(local_data)
+        }
+        else{
+            window.localStorage.setItem('data_users',JSON.stringify(data))
+            Print_Table(data)
         }
     })
 })
@@ -20,7 +26,7 @@ const Send_Form = () => {
     const email = document.getElementsByName('email')[0].value
     const first_name = document.getElementsByName('first_name')[0].value
     const last_name = document.getElementsByName('last_name')[0].value
-    const birthday = document.getElementsByName('date_of_birthday')[0].value
+    const date_of_birthday = document.getElementsByName('date_of_birthday')[0].value
 
     const Clear_Fields = () => {
         document.getElementsByName('email')[0].value = ''
@@ -29,20 +35,27 @@ const Send_Form = () => {
         document.getElementsByName('date_of_birthday')[0].value = ''
     }
 
-    if (email === '' || first_name === '' || last_name === '' || birthday === ''){
+    if (email === '' || first_name === '' || last_name === '' || date_of_birthday === ''){ // check all field
         alert('campo vacio')
         return
     }
-    const new_user = {email,first_name,last_name,birthday}
-    if (local_data){
-        local_data.push(new_user)
-        window.localStorage.setItem('data_users',JSON.stringify(local_data))
-        Print_Table(local_data)
+
+    const re = new RegExp('^[a-zA-Z0-9.!#$%&’*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$') // regular expression to validate email field
+    if (!re.test(email)){
+        alert('email no valido')
+        return
     }
-    else{
-        window.localStorage.setItem('data_users',JSON.stringify([new_user]))
-        Print_Table([new_user])
-    }
+
+    const dob = new Date(date_of_birthday);   // converting the age
+    const month_diff = Date.now() - dob.getTime();  
+    const age_dt = new Date(month_diff);   
+    const year = age_dt.getUTCFullYear();  
+    const birthday = Math.abs(year - 1970); 
+
+    const new_user = {'id':local_data.length+1,email,first_name,last_name,birthday}
+    local_data.push(new_user)
+    window.localStorage.setItem('data_users',JSON.stringify(local_data))
+    Print_Table([new_user])
     Clear_Fields()
 }
 
